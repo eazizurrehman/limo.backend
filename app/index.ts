4496646a9d71164b411a3b11ecee963d5285b52a
Aziz Ur Rehman <hello@azizurrehman.com>
2026-05-13 07:42:58 +0500

    feat: add morgan logger

--- app/index.ts
+++ app/index.ts
@@ -1,5 +1,6 @@
 import type { Express } from "express";
 import express from "express";
+import morgan from "morgan";
 import { authRouter } from "@/app/auth/routes";
 import { vehiclesRouter } from "@/app/vehicles/routes";
 import { ApiResponse } from "@/lib/api-response";
@@ -9,6 +10,7 @@ export function createApplication(): Express {
   const app = express();
 
   app.use(express.json());
+  app.use(morgan("tiny"));
 
   app.get("/health", (_, res) => {
     return ApiResponse.ok(res, "App is healthy!");
